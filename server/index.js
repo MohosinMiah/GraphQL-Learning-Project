a17ab44535e63c279ee5b3434d9ba8e3c1bacca8
mohosin16103308 <hamza1610330816@gmail.com>
2023-01-29 16:29:18 +0600

    MongoDb Connected

--- server/index.js
+++ server/index.js
@@ -4,13 +4,19 @@ const {graphqlHTTP} = require('express-graphql');
 
 const schema = require('./schema/schema.js');
 const port = process.env.PORT || 5000;
-
+const connectDB = require('./schema/config/db.js');
 
 var app = express();
+
+// Connect To Mongo DB
+connectDB();
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true,
 }));
+
+
 
 app.listen(port, console.log(`Server Running on ${port}`));
 
---
+++ server/schema/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    const connection = await mongoose.connect( process.env.MONGO_URI );
+    console.log( `${connection} Mongo DB Connected`)
+}
+
+
+module.exports = connectDB
