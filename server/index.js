0dae8a535044e6751a59abcf62a7b4444cbf4d41
mohosin16103308 <hamza1610330816@gmail.com>
2023-01-29 10:44:15 +0600

    Client by ID and Client List Display From Demo Data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 require('dotenv').config()
+const {graphqlHTTP} = require('express-graphql');
 
+const schema = require('./schema/schema.js');
 const port = process.env.PORT || 5000;
 
-const app = express();
 
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
+}));
 
 app.listen(port, console.log(`Server Running on ${port}`));
 
